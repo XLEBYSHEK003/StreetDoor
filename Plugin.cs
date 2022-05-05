8a4086ad2b886689ba3a3d3aec1918d5220c412d
Klarulor <55636688+Klarulor@users.noreply.github.com>
2022-05-05 20:31:01 +0300

    Fuck urself

--- Plugin.cs
+++ Plugin.cs
@@ -6,60 +6,46 @@ namespace DoorStreet
 {
     internal class Plugin : Plugin<Config>
     {
-        public override string Prefix
-        {
-            get
-            {
-                return "StreetDoor";
-            }
-        }
+        public override string Prefix { get; } = "StreetDoor";
 
+        public override string Name{ get; } = "StreetDoor";
 
-        public override string Name
-        {
-            get
-            {
-                return "StreetDoor";
-            }
-        }
-
-        public override string Author
-        {
-            get
-            {
-                return "XLEB_YSHEK";
-            }
-        }
+        public override string Author{ get; } = "XLEB_YSHEK";
 
         public override Version Version { get; } = new Version(2, 2, 8);
 
         public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
 
+
+        internal readonly EventHandler EventHandler = new EventHandler();        
+        internal readonly Plugin Instance;
+        public Plugin(){
+            Instance = this;
+        }
+
         public override void OnEnabled()
         {
-            this.handlers = new EventHandler(this);
-            Log.Info(string.Format("Plugin {0} ({1}) by {2} enabled sucessfully!", this.Name, this.Version, this.Author));
-            this.RegisterEvents();
+            Log.Info(string.Format("Plugin {0} ({1}) by {2} enabled sucessfully!", Name, Version, Author));
+            RegisterEvents();
         }
 
         public override void OnDisabled()
         {
-            Log.Info(string.Format("Плагин {0} ({1}) by {2} Выключен!", this.Name, this.Version, this.Author));
-            this.UnregisterEvents();
+            Log.Info(string.Format("Плагин {0} ({1}) by {2} Выключен!", Name, Version, Author));
+            UnregisterEvents();
         }
 
         public void RegisterEvents()
         {
-            Exiled.Events.Handlers.Server.RoundStarted += this.handlers.OnRoundStart;
-            Exiled.Events.Handlers.Server.WaitingForPlayers += this.handlers.OnWaitingForPlayers;
+            Exiled.Events.Handlers.Server.RoundStarted += EventHandler.OnRoundStart;
+            Exiled.Events.Handlers.Server.WaitingForPlayers += EventHandler.OnWaitingForPlayers;
         }
 
         public void UnregisterEvents()
         {
-            Exiled.Events.Handlers.Server.RoundStarted -= this.handlers.OnRoundStart;
-            Exiled.Events.Handlers.Server.WaitingForPlayers -= this.handlers.OnWaitingForPlayers;
+            Exiled.Events.Handlers.Server.RoundStarted -= EventHandler.OnRoundStart;
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= EventHandler.OnWaitingForPlayers;
         }
 
-        public EventHandler handlers;
     }
 }
